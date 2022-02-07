2a9d6638b2b129f5c228a8a1bdab215d8daf5275
Mylena <mylena49@hotmail.com>
2022-02-07 16:52:07 -0300

    first-commit

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [lista, setLista] = useState([]);
+
+  function inputUsuario(event) {
+    setInput(event.target.value);
+  }
+
+  function addLista() {
+    setLista((prevItens) => {
+      return [...prevItens, input];
+    });
+    setInput("");
+  }
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={inputUsuario} value={input} type="text" />
+        <button onClick={addLista}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {lista.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
